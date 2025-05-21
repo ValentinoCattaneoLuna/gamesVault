97420029acbf8f5cfed8f09d1511423c5afad356
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-21 16:56:13 -0300

    feat: implementacion de navegacion entre pantallas

---
+++ app/src/main/java/com/example/gamesvault/ui/screens/NavigationStack.kt
@@ -0,0 +1,30 @@
+package com.example.gamesvault.ui.screens
+
+import android.window.SplashScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.gamesvault.ui.screens.gamesvaultHome.GamesVaultHome
+import com.example.gamesvault.ui.screens.splash.GamesVaultSplash
+
+@Composable
+fun NavigationStack(){
+
+    val navController = rememberNavController()
+
+
+    NavHost(
+        navController = navController,
+        startDestination = Screens.GamesVaultSplash.route
+    ){
+
+        composable(route = Screens.GamesVaultSplash.route) {
+            GamesVaultSplash(navController = navController)
+        }
+        composable(route = Screens.GamesVaultHome.route) {
+            GamesVaultHome()
+        }
+    }
+
+}
