b094600d2c432360361a40f1af4f6b84570b9144
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-21 16:32:24 -0300

    feat: funciones mostrar la lista de juegos en tendencia y buscar juegos

--- app/src/main/java/com/example/gamesvault/data/GamesAPIDataSource.kt
+++ app/src/main/java/com/example/gamesvault/data/GamesAPIDataSource.kt
@@ -1,10 +1,32 @@
 package com.example.gamesvault.data
 
+import android.util.Log
 import com.example.gamesvault.domain.IGamesDataSource
 
 class GamesAPIDataSource : IGamesDataSource {
+
+
+
     override
     suspend fun getGamesList(): List<JuegoSummary> {
-        TODO("Not yet implemented")
+        return try {
+            val apiResponse = RetrofitInstance.GamesAPI.getGamesTrending()
+            apiResponse
+        } catch (e: Exception) {
+            Log.d("getGamesList", "Error fetching games: ${e.message}", e)
+            emptyList()
+        }
     }
+
+    override
+    suspend fun searchGames(query: String): List<JuegoSummary> {
+        return try {
+            val allGames = RetrofitInstance.GamesAPI.getGamesTrending()
+            allGames.filter { it.title.contains(query, ignoreCase = true) }
+        } catch (e: Exception) {
+            Log.d("searchGames", "Error searching games: ${e.message}", e)
+            emptyList()
+        }
+    }
+
 }
