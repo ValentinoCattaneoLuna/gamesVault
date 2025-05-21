f26d2af03c0c4205de8e472c31a7799dee882d96
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-21 16:56:32 -0300

    feat: implementacion del NavStack

--- app/src/main/java/com/example/gamesvault/MainActivity.kt
+++ app/src/main/java/com/example/gamesvault/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.gamesvault.ui.screens.NavigationStack
 import com.example.gamesvault.ui.screens.gamesvaultHome.GamesVaultHome
 import com.example.gamesvault.ui.theme.GamesVaultTheme
 
@@ -18,9 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GamesVaultTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GamesVaultHome(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavigationStack()
                 }
             }
         }
