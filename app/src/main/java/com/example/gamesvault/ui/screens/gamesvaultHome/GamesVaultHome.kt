24c43951e8cad1d326b29b6c5c68c8ec8f35dd55
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-21 16:34:31 -0300

    feat: se implemento la barra de busqueda de juegos, y una validacion de si hay datos a mostrar o no

--- app/src/main/java/com/example/gamesvault/ui/screens/gamesvaultHome/GamesVaultHome.kt
+++ app/src/main/java/com/example/gamesvault/ui/screens/gamesvaultHome/GamesVaultHome.kt
@@ -1,11 +1,18 @@
 package com.example.gamesvault.ui.screens.gamesvaultHome
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.gamesvault.ui.commons.GamesUIList
@@ -22,7 +29,36 @@ fun GamesVaultHome(
             style = MaterialTheme.typography.titleLarge
         )
 
-        GamesUIList(vm.uiState.gamesList, Modifier.fillMaxSize())
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            TextField(
+                value = vm.uiState.searchQuery,
+                modifier = Modifier.weight(1f),
+                label = {Text("Buscar juegos...")},
+                singleLine = true,
+                onValueChange = { vm.searchChange(it) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = { vm.searchGames()}
+            ) {
+                Text("Buscar")
+            }
+        }
+
+        if (vm.uiState.gamesList.isEmpty()) {
+            Text(
+                text = "Juegos no encontrados...",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 16.dp)
+            )
+        } else {
+            // Si hay juegos en la lista, se muestran
+            GamesUIList(vm.uiState.gamesList, Modifier.fillMaxSize())
+        }
+
     }
 
 }
