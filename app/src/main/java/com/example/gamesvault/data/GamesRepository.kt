01cdf0639e05a88c82dfbef8f9df964d677edb09
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-21 16:33:46 -0300

    feat: se implemento searchGames

--- app/src/main/java/com/example/gamesvault/data/GamesRepository.kt
+++ app/src/main/java/com/example/gamesvault/data/GamesRepository.kt
@@ -2,11 +2,18 @@ package com.example.gamesvault.data
 
 import com.example.gamesvault.domain.IGamesDataSource
 import com.example.gamesvault.domain.IGamesRepository
+import retrofit2.http.Query
 
-class GamesRepository(val gamesDataSource: IGamesDataSource = GamesDataSource()) : IGamesRepository {
+class GamesRepository(val gamesDataSource: IGamesDataSource = GamesAPIDataSource()) : IGamesRepository {
 
     override
     suspend fun fetchGames(): List<JuegoSummary>{
         return gamesDataSource.getGamesList()
     }
+
+    override
+    suspend fun searchGames(query: String): List<JuegoSummary>{
+        return gamesDataSource.searchGames(query)
+    }
+
 }
