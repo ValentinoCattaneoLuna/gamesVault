813b691042add853467dbf32cc0ddccd096d01d0
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-28 02:26:19 -0300

    feat: state de perfil

---
+++ app/src/main/java/com/example/gamesvault/ui/screens/perfil/GamesVaultPerfilState.kt
@@ -0,0 +1,12 @@
+package com.example.gamesvault.ui.screens.perfil
+
+
+data class GamesVaultPerfilState (
+
+    val username: String = "",
+    val email: String = "",
+    val foto_url: String = "",
+    val creacion_cuenta: Long = 0L,
+    val ultimo_login: Long = 0L
+
+)
