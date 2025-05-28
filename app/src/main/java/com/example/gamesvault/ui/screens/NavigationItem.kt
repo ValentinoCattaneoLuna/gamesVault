d8d239262d53891f6b664fbf4c38eeeb45427996
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-28 02:27:34 -0300

    feat: clase de item de la barra de navegacion

---
+++ app/src/main/java/com/example/gamesvault/ui/screens/NavigationItem.kt
@@ -0,0 +1,11 @@
+package com.example.gamesvault.ui.screens
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val titulo: String,
+    val icono: ImageVector,
+    val route: String
+)
+
+
