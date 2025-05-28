fe930690aaf369f81efa9f763d7960381cdf6e1e
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-28 02:27:22 -0300

    feat: items de la barra de navegacion

---
+++ app/src/main/java/com/example/gamesvault/ui/screens/NavigationItems.kt
@@ -0,0 +1,30 @@
+package com.example.gamesvault.ui.screens
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Shuffle
+
+val navigationItems = listOf(
+    NavigationItem(
+        titulo = "Inicio",
+        icono = Icons.Default.Home,
+        route = Screens.GamesVaultHome.route
+    ),
+    NavigationItem(
+        titulo = "Favoritos",
+        icono = Icons.Default.FavoriteBorder,
+        route = ""
+    ),
+    NavigationItem(
+        titulo = "Aleatorio",
+        icono = Icons.Default.Shuffle,
+        route = ""
+    ),
+    NavigationItem(
+        titulo = "Perfil",
+        icono = Icons.Default.Person,
+        route = Screens.GamesVaultPerfil.route
+    )
+)
