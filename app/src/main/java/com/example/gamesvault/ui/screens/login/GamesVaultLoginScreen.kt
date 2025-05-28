0e4b4e1713eb32556ca5864ae7f7a2da2ef956e9
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-28 02:24:58 -0300

    feat: pantalla de login

---
+++ app/src/main/java/com/example/gamesvault/ui/screens/login/GamesVaultLoginScreen.kt
@@ -0,0 +1,250 @@
+package com.example.gamesvault.ui.screens.login
+
+import android.media.Image
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.gamesvault.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox  // Ejemplo, busca el de Google
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import com.example.gamesvault.R.drawable.ic_google
+import com.example.gamesvault.R.drawable.games_vault_logo
+
+
+@Composable
+fun GamesVaultLogin(
+    onGoogleLoginClick: ()-> Unit,
+    modifier: Modifier = Modifier,
+    navController: NavHostController
+
+){
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background( Color(0xFF020817))
+    ){
+        Column(
+            modifier = Modifier
+            .fillMaxHeight()
+            //.background(Color(0xFF020817))
+            .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.games_vault_logo),
+            contentDescription = "Logo de la app",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(100.dp)
+        )
+        // Título
+        Text(
+            text = "Games Vault",
+            color = Color.White,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 40.dp)
+        )
+
+        // Botones de navegación
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Iniciar Sesión",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = "Registrarse",
+                color = Color(0xFF94A3B8),
+                fontSize = 20.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // Campo de email
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Email",
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent)
+                    .border(
+                        width = 1.dp,
+                        color = if (email.value.isNotEmpty()) Color.White else Color(0xFF94A3B8),
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+            ) {
+                BasicTextField(
+                    value =  email.value,
+                    onValueChange = { email.value = it },
+                    textStyle = TextStyle(color = Color.White),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Campo de contraseña
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Contraseña",
+                color = Color(0xFF94A3B8),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent)
+                    .border(
+                        width = 1.dp,
+                        color = if (password.value.isNotEmpty()) Color.White else Color(0xFF94A3B8),
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+            ) {
+                BasicTextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    textStyle = TextStyle(color = Color.White),
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Botón de iniciar sesión
+        Button(
+            onClick = {  },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF7C3AED),
+                contentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "Iniciar Sesión",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Divider con texto
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            HorizontalDivider(
+                color = Color(0xFF94A3B8),
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = "O continuar con",
+                color = Color(0xFF94A3B8),
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+
+            HorizontalDivider(
+                color = Color(0xFF94A3B8),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Botón de Google
+        Button(
+            onClick = { onGoogleLoginClick() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF020817),
+                contentColor = Color.White
+            ),
+            border = BorderStroke(1.dp, Color(0xFF94A3B8))
+        ) {
+            Image(
+                painter = painterResource(id = ic_google),
+                contentDescription = "Google logo",
+                modifier = Modifier.size(24.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Google",
+                fontSize = 16.sp
+            )
+        }
+    }}
+
+}
+
