8088a883c8c1f0cf90d77ca4c8928867e5381cf3
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-05-28 03:30:58 -0300

    feat: valores x defecto

--- app/src/main/java/com/example/gamesvault/data/Juegos.kt
+++ app/src/main/java/com/example/gamesvault/data/Juegos.kt
@@ -5,48 +5,48 @@ import java.time.LocalDate
 import java.util.Date
 
 data class JuegoSummary(
-    val id: Int,
-    val title: String,
+    val id: Int = 0,
+    val title: String = "",
     @SerializedName("thumbnail")
-    val urlImagen: String,
+    val urlImagen: String = "",
     @SerializedName("game_url")
-    val urlJuego: String,
+    val urlJuego: String = "",
     @SerializedName("short_description")
-    val descripcionCorta: String,
-    val genre: String,
-    val platform: String
+    val descripcionCorta: String = "",
+    val genre: String = "",
+    val platform: String = ""
 )
 
 data class JuegoDetail(
-    val id: Int,
-    val title: String,
+    val id: Int = 0,
+    val title: String = "",
     @SerializedName("thumbnail")
-    val urlImagen: String,
+    val urlImagen: String = "",
     @SerializedName("game_url")
-    val urlJuego: String,
+    val urlJuego: String = "",
     @SerializedName("description")
-    val descripcionCompleta: String,
-    val genre: String,
-    val platform: String,
+    val descripcionCompleta: String = "",
+    val genre: String = "",
+    val platform: String = "",
     @SerializedName("release_date")
-    val fechaCreacion : String,
+    val fechaCreacion: String = "",
     @SerializedName("minimum_system_requirements")
-    val requerimientosMinimos: ReqMin,
-    val screenshots: List<Screenshot>
-
+    val requerimientosMinimos: ReqMin = ReqMin(),
+    val screenshots: List<Screenshot> = emptyList()
 )
 
+
 data class ReqMin(
-    val os: String,
-    val processor: String,
-    val memory: String,
-    val graphics: String,
-    val storage: String
+    val os: String = "",
+    val processor: String = "",
+    val memory: String = "",
+    val graphics: String = "",
+    val storage: String = ""
 )
 
 data class Screenshot(
-    val id: Int,
-    val image: String
+    val id: Int = 0,
+    val image: String = ""
 )
 
 
