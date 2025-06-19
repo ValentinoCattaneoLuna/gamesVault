54f315db0d3f1dc9dc75048760224ada3567cb09
ValentinoCattaneoLuna <valencattaneo04@gmail.com>
2025-06-19 16:20:13 -0300

    feat: funciones para cacheado en la dblocal

--- app/src/main/java/com/example/gamesvault/data/GamesRepository.kt
+++ app/src/main/java/com/example/gamesvault/data/GamesRepository.kt
@@ -1,58 +1,29 @@
-package com.example.gamesvault.data
+package com.example.gamesvault.dataAdd
+
+import com.example.gamesvault.data.GamesAPIDataSource
+import com.example.gamesvault.data.JuegoDetail
+import com.example.gamesvault.data.JuegoSummary
+
 
 import com.example.gamesvault.domain.IGamesDataSource
-import com.example.gamesvault.domain.IGamesLocalDataSource
+
 import com.example.gamesvault.domain.IGamesRepository
-import javax.inject.Inject
+import retrofit2.http.Query
 
-class GamesRepository @Inject constructor(
-    private val remoteDataSource: IGamesDataSource,
-    private val localDataSource: IGamesLocalDataSource
-) : IGamesRepository {
+class GamesRepository(val gamesDataSource: IGamesDataSource = GamesAPIDataSource()) : IGamesRepository {
 
-    override suspend fun fetchGames(): List<JuegoSummary> {
-        return try {
-            // 1. Obtener de API
-            val remoteGames = remoteDataSource.getGamesList()
+    override
+    suspend fun fetchGames(): List<JuegoSummary>{
+        return gamesDataSource.getGamesList()
+    }
 
-            // 2. Guardar en caché (en segundo plano)
-            localDataSource.cacheGames(remoteGames)
+    override
+    suspend fun searchGames(query: String): List<JuegoSummary>{
+        return gamesDataSource.searchGames(query)
 
-            // 3. Devolver datos frescos
-            remoteGames
-        } catch (e: Exception) {
-            // Fallback a caché si hay error
-            localDataSource.getCachedGames().takeIf { it.isNotEmpty() }
-                ?: throw e // Relanzar si no hay caché
-        }
     }
 
     override suspend fun fetchGame(juegoId: Int): JuegoDetail {
-        return localDataSource.getGameDetail(juegoId)?.let {
-            // Devolver caché si existe
-            it
-        } ?: run {
-            // Obtener de API si no hay caché
-            val remoteGame = remoteDataSource.getGameById(juegoId)
-            localDataSource.cacheGameDetail(remoteGame)
-            remoteGame
+        return gamesDataSource.getGameById(juegoId)
         }
     }
-
-    override suspend fun searchGames(query: String): List<JuegoSummary> {
-        val localResults = localDataSource.searchCachedGames(query)
-        return if (localResults.isNotEmpty()) {
-            localResults
-        } else {
-            remoteDataSource.searchGames(query).also {
-                localDataSource.cacheGames(it) // cachear resultados de búsqueda
-            }
-        }
-    }
-
-    //Método para actualizar datos
-    suspend fun refreshGames() {
-        val remoteGames = remoteDataSource.getGamesList()
-        localDataSource.cacheGames(remoteGames)
-    }
-}
