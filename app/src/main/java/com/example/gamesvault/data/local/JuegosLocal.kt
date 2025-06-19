2d3345e3cb5e06ab333b3604902daccc641b52c7
ValentinoCattaneoLuna <valencattaneo04@gmail.com>
2025-06-19 16:16:07 -0300

    fix: correccion de estructura para el cacheado de los juegos

--- app/src/main/java/com/example/gamesvault/data/local/JuegosLocal.kt
+++ app/src/main/java/com/example/gamesvault/data/local/JuegosLocal.kt
@@ -2,22 +2,7 @@ package com.example.gamesvault.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.gamesvault.data.ReqMin
-import com.example.gamesvault.data.Screenshot
-import com.google.gson.annotations.SerializedName
-// JuegosLocal.kt
-@Entity(tableName = "juegos_summary")
-data class JuegoSummaryLocal(
-    @PrimaryKey val id: Int,
-    val title: String,
-    val urlImagen: String,
-    val urlJuego: String,
-    val descripcionCorta: String,
-    val genre: String,
-    val platform: String,
-    val isFavorite: Boolean = false,
-    val lastUpdated: Long = System.currentTimeMillis()
-)
+
 
 @Entity(tableName = "juegos_detail")
 data class JuegoDetailLocal(
