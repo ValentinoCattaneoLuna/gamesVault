a10679aae48a3b37877562163ae2b901f91338b6
ValentinoCattaneoLuna <valencattaneo04@gmail.com>
2025-06-19 16:14:31 -0300

    fix: correccion de estructura para el cacheado de los juegos

--- app/src/main/java/com/example/gamesvault/data/local/ModelMapping.kt
+++ app/src/main/java/com/example/gamesvault/data/local/ModelMapping.kt
@@ -1,29 +1,36 @@
 package com.example.gamesvault.data.local
 
+import com.example.gamesvault.data.JuegoDetail
 import com.example.gamesvault.data.JuegoSummary
 import com.google.gson.annotations.SerializedName
 import kotlin.Int
 
-fun JuegoSummary.toJuegoSummaryLocal(): JuegoSummaryLocal {
-    return JuegoSummaryLocal(
+fun JuegoDetail.toJuegoDetailLocal(): JuegoDetailLocal {
+    return JuegoDetailLocal(
         id = this.id,
         title = this.title,
         urlImagen = this.urlImagen,
         urlJuego = this.urlJuego,
-        descripcionCorta = this.descripcionCorta,
+        descripcionCompleta = this.descripcionCompleta,
         genre = this.genre,
-        platform = this.platform
+        platform = this.platform,
+        fechaCreacion = this.fechaCreacion,
+        requerimientosMinimos = fromReqMin(this.requerimientosMinimos),
+        screenshots = fromScreenshots(this.screenshots)
     )
 }
 
-fun JuegoSummaryLocal.toJuegoSummary(): JuegoSummary {
-    return JuegoSummary(
+fun JuegoDetailLocal.toJuegoDetail(): JuegoDetail {
+    return JuegoDetail(
         id = this.id,
         title = this.title,
         urlImagen = this.urlImagen,
         urlJuego = this.urlJuego,
-        descripcionCorta = this.descripcionCorta,
+        descripcionCompleta = this.descripcionCompleta,
         genre = this.genre,
-        platform = this.platform
+        platform = this.platform,
+        fechaCreacion = this.fechaCreacion,
+        requerimientosMinimos = toReqMin(this.requerimientosMinimos),
+        screenshots = toScreenshots(this.screenshots)
     )
 }
