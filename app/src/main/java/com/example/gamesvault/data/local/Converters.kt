b46f9ee6ed78291bb2ea222db03463583d4d95da
ValentinoCattaneoLuna <valencattaneo04@gmail.com>
2025-06-19 16:18:50 -0300

    feat: se elimino el decorador typeconverter

--- app/src/main/java/com/example/gamesvault/data/local/Converters.kt
+++ app/src/main/java/com/example/gamesvault/data/local/Converters.kt
@@ -1,29 +1,23 @@
 package com.example.gamesvault.data.local
 
-import androidx.room.TypeConverter
+
 import com.example.gamesvault.data.ReqMin
 import com.example.gamesvault.data.Screenshot
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class Converters {
-    @TypeConverter
     fun fromReqMin(reqMin: ReqMin): String {
         return Gson().toJson(reqMin)
     }
 
-    @TypeConverter
     fun toReqMin(json: String): ReqMin {
         return Gson().fromJson(json, ReqMin::class.java)
     }
 
-    @TypeConverter
     fun fromScreenshots(list: List<Screenshot>): String {
         return Gson().toJson(list)
     }
 
-    @TypeConverter
     fun toScreenshots(json: String): List<Screenshot> {
         return Gson().fromJson(json, object : TypeToken<List<Screenshot>>() {}.type)
     }
-}
