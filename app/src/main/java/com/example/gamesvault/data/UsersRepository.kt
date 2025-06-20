af75414067787adb111297f36a03996e66cc9d35
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-20 16:24:46 -0300

    feat: implementacion de la interfaz de IUserRepository.kt

--- app/src/main/java/com/example/gamesvault/data/UsersRepository.kt
+++ app/src/main/java/com/example/gamesvault/data/UsersRepository.kt
@@ -1,8 +1,23 @@
 package com.example.gamesvault.data
 
-class UsersRepository() {
+import com.example.gamesvault.domain.IUserRepository
 
+class UsersRepository(
+    val usersDataSource: UsersDataSource = UsersDataSource())
+    : IUserRepository{
 
+    override
+    suspend fun fetchUser() :Usuario{
+        return usersDataSource.getUser()
+    }
 
+    override
+    suspend fun addFavorite(id:Int){
+        return usersDataSource.agregarFavorito(id)
+    }
 
+    override
+    suspend fun removeFavorite(id: Int){
+        return usersDataSource.eliminarFavorito(id)
+    }
 }
