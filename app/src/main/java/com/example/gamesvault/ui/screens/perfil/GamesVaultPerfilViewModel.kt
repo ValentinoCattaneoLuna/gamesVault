dff067e88778deb14093559881eeda85e59480e7
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-20 18:00:53 -0300

    feat: se agrego en la carga del usuario la cantidad de favoritos para actualizar el estado dependendiendo de los juegos favoritos

--- app/src/main/java/com/example/gamesvault/ui/screens/perfil/GamesVaultPerfilViewModel.kt
+++ app/src/main/java/com/example/gamesvault/ui/screens/perfil/GamesVaultPerfilViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.gamesvault.ui.screens.perfil
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gamesvault.data.UsersRepository
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import kotlin.math.log
 
-class GamesVaultPerfilViewModel : ViewModel() {
+class GamesVaultPerfilViewModel(
+    private val usersRepository: UsersRepository = UsersRepository()
+) : ViewModel() {
 
 
     var uiState by mutableStateOf(GamesVaultPerfilState())
@@ -16,15 +24,19 @@ class GamesVaultPerfilViewModel : ViewModel() {
     }
 
 
-    private fun getUserData(){
-        uiState = uiState.copy(
-            username = FirebaseAuth.getInstance().currentUser?.displayName ?: "usuario desconocido",
-            email = FirebaseAuth.getInstance().currentUser?.email ?: "correo@desconocido.com",
-            foto_url = FirebaseAuth.getInstance().currentUser?.photoUrl.toString() ?: "",
-            creacion_cuenta = FirebaseAuth.getInstance().currentUser?.metadata?.creationTimestamp ?: 0L ,
-            ultimo_login = FirebaseAuth.getInstance().currentUser?.metadata?.lastSignInTimestamp ?: 0L
-        )
-    }
+    private fun getUserData() {
+        viewModelScope.launch {
+            val user = usersRepository.fetchUser()
 
+            uiState = uiState.copy(
+                username = FirebaseAuth.getInstance().currentUser?.displayName ?: "usuario desconocido",
+                email = FirebaseAuth.getInstance().currentUser?.email ?: "correo@desconocido.com",
+                foto_url = FirebaseAuth.getInstance().currentUser?.photoUrl?.toString() ?: "",
+                creacion_cuenta = FirebaseAuth.getInstance().currentUser?.metadata?.creationTimestamp ?: 0L,
+                ultimo_login = FirebaseAuth.getInstance().currentUser?.metadata?.lastSignInTimestamp ?: 0L,
+                favoritos = user.estadisticas
+            )
+        }
+    }
 
 }
