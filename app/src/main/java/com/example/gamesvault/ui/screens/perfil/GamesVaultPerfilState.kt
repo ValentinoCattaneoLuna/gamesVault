fb9781f43c026215371b23be41c5b33fd70fbd85
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-20 18:00:01 -0300

    feat: se agrego la estadistica de favoritos

--- app/src/main/java/com/example/gamesvault/ui/screens/perfil/GamesVaultPerfilState.kt
+++ app/src/main/java/com/example/gamesvault/ui/screens/perfil/GamesVaultPerfilState.kt
@@ -7,6 +7,6 @@ data class GamesVaultPerfilState (
     val email: String = "",
     val foto_url: String = "",
     val creacion_cuenta: Long = 0L,
-    val ultimo_login: Long = 0L
-
+    val ultimo_login: Long = 0L,
+    val favoritos: Int = 0
 )
