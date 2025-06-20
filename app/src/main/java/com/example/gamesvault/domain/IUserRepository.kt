5f03e26c5c8a95e57eff212063b5777e747c7055
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-20 16:25:04 -0300

    feat: interfaz para el userRepository

---
+++ app/src/main/java/com/example/gamesvault/domain/IUserRepository.kt
@@ -0,0 +1,9 @@
+package com.example.gamesvault.domain
+
+import com.example.gamesvault.data.Usuario
+
+interface IUserRepository {
+    suspend fun fetchUser() :Usuario
+    suspend fun addFavorite(id:Int)
+    suspend fun removeFavorite(id: Int)
+}
